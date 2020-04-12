32b06a8cb34671b663b4400cfa37b56960e28c3d
krishnan-aed <krishnan.sa@husky.neu.edu>
2020-04-12 15:24:42 -0400

    adding some files

---
+++ finalProject_Santhosh/src/UserInterfacePackage/UpdateDeliverPersonJPanel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserInterfacePackage;
+
+import Business.User.User;
+import Business.User.UserDirectory;
+import java.awt.PopupMenu;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author imskr
+ */
+public class UpdateDeliverPersonJPanel extends PopupMenu {
+
+    public UpdateDeliverPersonJPanel(JPanel viewJPanel, User user, UserDirectory directory) {
+    }
+    
+}
